dd41a6f1e9a369c70c4b97f9828fa77f7687cfb9
Lucas Fernandes <lucasffb120@gmail.com>
2024-11-05 19:23:38 -0300

    Modularizando footer e header com fetch, adicionando nas páginas eventos.html e index.html

--- scripts/main.js
+++ scripts/main.js
@@ -8,4 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
             document.querySelector('header').innerHTML = data;
         })
         .catch(error => console.error(error));
+
+    fetch('../layouts/footer.html')
+        .then(response => {
+            if (!response.ok) throw new Error('Erro ao carregar o arquivo footer.html');
+            return response.text();
+        })
+        .then(data => {
+            document.querySelector('footer').innerHTML = data;
+        })
+        .catch(error => console.error(error));
 });
