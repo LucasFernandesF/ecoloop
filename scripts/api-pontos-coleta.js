a073b6083ba07ad06bc41f848b8f07908eafaf81
Lucas Fernandes <lucasffb120@gmail.com>
2024-11-05 19:14:58 -0300

    Refatorando api-pontos-coleta.js: modularizando funções de inicialização de mapa, localização do usuário e pontos de coleta

--- scripts/api-pontos-coleta.js
+++ scripts/api-pontos-coleta.js
@@ -1,54 +1,54 @@
-var watchID = navigator.geolocation.watchPosition(sucess, error)
-
+var watchID = navigator.geolocation.watchPosition(onSuccess, onError);
 var map;
 
 function generateGoogleMapsLink(destLat, destLng, userLat, userLng) {
     return `https://www.google.com/maps/dir/?api=1&origin=${userLat},${userLng}&destination=${destLat},${destLng}&travelmode=driving`;
 }
 
-function sucess(position) {
-    const userLatitude = position.coords.latitude;
-    const userLongitude = position.coords.longitude;
-    const pontosColeta = [
-        { id: 1, nome: "UBS Laranjeiras", latitude: -19.9443595, longitude: -44.1292374 },
-        { id: 2, nome: "Supermercado Apoio Mineiro Betim", latitude: -19.9762, longitude: -44.1960 }
-    ]
-
-    if (map === undefined) {
-        map = L.map('map').setView([userLatitude, userLongitude], 13);
-    } else {
-        map.remove();
-        map = L.map('map').setView([userLatitude, userLongitude], 13);
-    }
-
+function initializeMap(lat, long) {
+    map = L.map('map').setView([lat, long], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+}
 
-    L.marker([userLatitude, userLongitude]).addTo(map)
+function addUserLocation(lat, long) {
+    L.marker([lat, long]).addTo(map)
         .bindPopup('Localização atual')
         .openPopup();
+}
 
-
-    for (let index = 0; index < pontosColeta.length; index++) {
-        
-
-        L.marker([pontosColeta[index].latitude, pontosColeta[index].longitude]).addTo(map)
+function addCollectionPoints(userLat, userLng, pontos) {
+    pontos.forEach(point => {
+        L.marker([point.latitude, point.longitude]).addTo(map)
             .bindPopup(`
-            <b>${pontosColeta[index].nome}</b><br>
-            <a href="${generateGoogleMapsLink(pontosColeta[index].latitude, pontosColeta[index].longitude, userLatitude, userLongitude)}" target="_blank">
-             Ver rota no Google Maps
-             </a>
+                <b>${point.nome}</b><br>
+                <a href="${generateGoogleMapsLink(point.latitude, point.longitude, userLat, userLng)}" target="_blank">
+                    Ver rota no Google Maps
+                </a>
             `);
+    });
+}
+
+function onSuccess(position) {
+    const userLatitude = position.coords.latitude;
+    const userLongitude = position.coords.longitude;
+
+    const pontosColeta = [
+        { id: 1, nome: "UBS Laranjeiras", latitude: -19.9443595, longitude: -44.1292374 },
+        { id: 2, nome: "Supermercado Apoio Mineiro Betim", latitude: -19.9762, longitude: -44.1960 }
+    ];
+
+    if (!map) {
+        initializeMap(userLatitude, userLongitude);
+    } else {
+        map.setView([userLatitude, userLongitude], 13);
     }
+
+    addUserLocation(userLatitude, userLongitude);
+    addCollectionPoints(userLatitude, userLongitude, pontosColeta);
 }
 
-function error(err) {
-    console.log(err);
+function onError(err) {
+    console.log("Erro ao obter localização", err);
 }
-
-
-
-
-
-
