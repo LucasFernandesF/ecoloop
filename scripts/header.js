6760a9b857dd1e9ef2e0185f02ab799fa9e95028
Lucas Fernandes <lucasffb120@gmail.com>
2024-11-12 11:43:44 -0300

    Adicionando funcionalidade no icone de login quando o mouse passar na tela ou for clicado aparece um menu com links

---
+++ scripts/header.js
@@ -0,0 +1,12 @@
+const menuButton = document.querySelector('.menu-button');
+const menuContainer = document.querySelector('.menu-container');
+
+menuButton.addEventListener('click', () => {
+    menuContainer.classList.toggle('active');
+});
+
+document.addEventListener('click', (event) => {
+    if (!menuContainer.contains(event.target)) {
+        menuContainer.classList.remove('active');
+    }
+});
