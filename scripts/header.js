2a495b8eae5de984d48a82ec0d3f8e4e4f0d9e31
Lucas Fernandes <lucasffb120@gmail.com>
2024-11-21 14:42:34 -0300

    Refatorando o header.js retirando funcionalidades inutilizaveis

--- scripts/header.js
+++ scripts/header.js
@@ -2,44 +2,33 @@ import { auth, db } from './firebase.js';
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getDoc, doc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
-// Seleciona os elementos do DOM
-const menuButton = document.querySelector('.menu-button');
-const menuContainer = document.querySelector('.menu-container');
-
 document.addEventListener("DOMContentLoaded", () => {
-    const userLogado = document.querySelector('.email-logado');  
-    console.log(userLogado);  
+  const userLogado = document.querySelector('.email-logado');
+  console.log(userLogado);
 
-    onAuthStateChanged(auth, async (user) => {
-        if (user) {
-            const docRef = doc(db, "users", user.uid);
-            try {
-                const docSnap = await getDoc(docRef);
-                if (docSnap.exists()) {
-                    const userData = docSnap.data();
-                    const emailToDisplay = userData.email || user.email; 
-                    if (userLogado) {
-                        userLogado.textContent = emailToDisplay;
-                        userLogado.style.display = "block"; 
-                        console.log(user)
-                    }
-                }
-            } catch (error) {
-                console.error("Erro ao buscar dados do Firestore:", error);
-            }
-        } else {
-            console.log("Nenhum usuário logado.");
-            if (userLogado) {
-                userLogado.style.display = "none"; 
-            }
+  onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      const docRef = doc(db, "users", user.uid);
+      try {
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const userData = docSnap.data();
+          const emailToDisplay = userData.email || user.email;
+          if (userLogado) {
+            userLogado.textContent = emailToDisplay;
+            userLogado.style.display = "block";
+            console.log(user)
+          }
         }
-    });
+      } catch (error) {
+        console.error("Erro ao buscar dados do Firestore:", error);
+      }
+    } else {
+      console.log("Nenhum usuário logado.");
+      if (userLogado) {
+        userLogado.style.display = "none";
+      }
+    }
+  });
 });
 
-menuButton.addEventListener('mouseover', () => {
-  menuContainer.classList.add('active');
-});
-
-menuContainer.addEventListener('mouseleave', () => {
-  menuContainer.classList.remove('active');
-});
