7c29827f7492e0511354bd42b52d1dae37c667a5
Lucas Ferreira <45069020+lucsferreira@users.noreply.github.com>
2024-11-27 16:57:47 -0300

    Melhorias Visuais - Lucas Ferreira

--- scripts/login.js
+++ scripts/login.js
@@ -8,11 +8,7 @@ export async function loginEmail() {
     try {
         const userCredential = await signInWithEmailAndPassword(auth, email, password);
         
-        alert("Login realizado com sucesso!");
-
-        setTimeout(() => {
-            window.location.href = "../html/minha-conta.html";
-        }, 2000);
+        showAlert("Login realizado com sucesso!");
     } catch (error) {
         console.error("Erro ao logar: ", error.message);
         alert("Erro ao logar: " + error.message);
@@ -22,4 +18,29 @@ export async function loginEmail() {
 document.getElementById("login-form").addEventListener("submit", (e) => {
     e.preventDefault();
     loginEmail();
+});
+
+// Função para abrir o modal
+function showAlert(message) {
+    $('#modal-message').text(message); // Atualiza a mensagem do modal
+    $('#custom-modal').modal('show'); // Abre o modal
+}
+
+// Função para fechar o modal ao clicar no botão ou no 'X'
+$('#modal-close-btn, #modal-close').on('click', function () {
+    setTimeout(() => {
+        window.location.href = '../html/minha-conta.html';
+    }, 2000);
+});
+
+document.addEventListener("DOMContentLoaded", function () {        
+    fetch('../layouts/alerta.html')
+        .then(response => {
+            if (!response.ok) throw new Error('Erro ao carregar o arquivo footer.html');
+            return response.text();
+        })
+        .then(data => {
+            document.querySelector('alerta').innerHTML = data;
+        })
+        .catch(error => console.error(error));
 });
--- scripts/main.js
+++ scripts/main.js
@@ -18,5 +18,36 @@ document.addEventListener("DOMContentLoaded", function () {
             document.querySelector('footer').innerHTML = data;
         })
         .catch(error => console.error(error));
+        
+    fetch('../layouts/header.html')
+        .then(response => {
+            if (!response.ok) throw new Error('Erro ao carregar o arquivo footer.html');
+            return response.text();
+        })
+        .then(data => {
+            document.querySelector('head').innerHTML = data;
+        })
+        .catch(error => console.error(error));
 });
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+    // Exibir o botão após o scroll
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 300) {
+            scrollToTopBtn.style.display = 'block';
+        } else {
+            scrollToTopBtn.style.display = 'none';
+        }
+    });
+
+    // Scroll suave ao topo
+    scrollToTopBtn.addEventListener('click', function () {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
